1a5565170629bd26210b572828d3534e1471aa2c
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-01-19 12:26:33 +0545

    Add files via upload
    Normal basic operations of array is described here.

---
+++ Array.js
@@ -0,0 +1,20 @@
+const friends = ['Micheal',  'Steven', 'Peter'];
+
+console.log(friends);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length-1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+//You can also be able to insert the whole array into any new array
+//See this example
+
+const firstName = 'Jonas';
+const Shishi = [firstName, 'Thakur', 2021-1997, 'teacher', friends];
+console.log(Shishi);
+console.log(Shishi.length);
